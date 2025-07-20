62c0a88cf615ed31a6a6905acd614075f2a75983
VesperVanity <OCEAN.jung@protonmail.com>
2025-07-20 23:36:31 +0200

    Add chapter 1 dir
    Finish exercise 1 and 2

---
+++ chapter_1/hello_world.c
@@ -0,0 +1,13 @@
+/*
+The C Programming Language by Brian W. Kernighan & Dennis M. Ritchie
+Chapter 1 Exercise 1-1, 1-2 
+File created: 2025-07-20
+File last modified: 2025-07-20
+Author: VesperVanity (VV) https://github.com/VesperVanity
+*/
+#include <stdio.h>
+
+int main()
+{
+	printf("Hello, World!\n");
+}
