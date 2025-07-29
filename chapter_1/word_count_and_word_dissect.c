9373fcb6f673a55cf781632deb146384e48db8f7
VesperVanity <OCEAN.jung@protonmail.com>
2025-07-29 21:28:15 +0200

    Add reminder

---
+++ chapter_1/word_count_and_word_dissect.c
@@ -0,0 +1,14 @@
+/*
+The C Programming Language by Brian W. Kernighan & Dennis M. Ritchie
+Chapter 1 Exercise
+File created: 2025-07-29
+File last modified: 2025-07-29
+Author: VesperVanity (VV) https://github.com/VesperVanity
+*/
+#include <stdio.h>
+
+int main(void)
+{
+	printf("%s\n", "Add exercise number to comment.");
+	return 0;
+}
