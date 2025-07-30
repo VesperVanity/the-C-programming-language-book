d0b9bea27f5a21a6d855d770fbc0af5e3842ae0f
VesperVanity <OCEAN.jung@protonmail.com>
2025-07-30 20:20:02 +0200

    Start exercise 1-11

--- chapter_1/word_count_and_word_dissect.c
+++ chapter_1/word_count_and_word_dissect.c
@@ -1,8 +1,8 @@
 /*
 The C Programming Language by Brian W. Kernighan & Dennis M. Ritchie
-Chapter 1 Exercise
+Chapter 1 Exercise - 
 File created: 2025-07-29
-File last modified: 2025-07-29
+File last modified: 2025-07-30
 Author: VesperVanity (VV) https://github.com/VesperVanity
 */
 #include <stdio.h>
@@ -10,5 +10,6 @@ Author: VesperVanity (VV) https://github.com/VesperVanity
 int main(void)
 {
 	printf("%s\n", "Add exercise number to comment.");
+	printf("%s\n", "Start exercise 1-11.");
 	return 0;
 }
