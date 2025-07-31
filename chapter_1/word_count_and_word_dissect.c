f65440dd0cb36c9d9ef0c12e1e0b0cf88b6e4bf2
VesperVanity <OCEAN.jung@protonmail.com>
2025-07-31 20:31:14 +0200

    Exercise 1-11

--- chapter_1/word_count_and_word_dissect.c
+++ chapter_1/word_count_and_word_dissect.c
@@ -1,15 +1,45 @@
 /*
 The C Programming Language by Brian W. Kernighan & Dennis M. Ritchie
-Chapter 1 Exercise - 
+Chapter 1 Exercise - 1-11, 1-12
 File created: 2025-07-29
-File last modified: 2025-07-30
+File last modified: 2025-07-31
 Author: VesperVanity (VV) https://github.com/VesperVanity
 */
 #include <stdio.h>
 
+void input_one_word_per_line();
+
 int main(void)
 {
-	printf("%s\n", "Add exercise number to comment.");
-	printf("%s\n", "Start exercise 1-11.");
+	input_one_word_per_line();
 	return 0;
+}
+
+void input_one_word_per_line()
+{
+	//Too dumb for this
+	//Print a new line each blank
+	int c;
+	int word_done = 0;
+	while((c = getchar()) != EOF)
+	{
+		if(c == ' ' || c == '\n' || c == '\t')
+		{
+			if(word_done == 1)
+			{
+				word_done = 0;
+				putchar('\n');
+			}
+			
+		}
+		else if(word_done == 0)
+		{
+			word_done = 1;
+			putchar(c);
+		}
+		else
+		{
+			putchar(c);
+		}
+	}
 }
