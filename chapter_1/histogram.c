d3893264d69fa7e81ee64d00766eb2e4c6947b7e
VesperVanity <OCEAN.jung@protonmail.com>
2025-08-01 01:36:30 +0200

    Exercise 1-13

---
+++ chapter_1/histogram.c
@@ -0,0 +1,41 @@
+/*
+The C Programming Language by Brian W. Kernighan & Dennis M. Ritchie
+Chapter 1 Exercise - 1-13, 1-14
+File created: 2025-08-01
+File last modified: 2025-08-01
+Author: VesperVanity (VV) https://github.com/VesperVanity
+*/
+#include <stdio.h>
+
+void histogram_horizontal();
+
+int main(void)
+{
+	histogram_horizontal();
+	return 0;
+}
+
+void histogram_horizontal()
+{
+	//Print histogram horizontally depending on the length of words of the input
+	//Use arrays or counter variable
+	int c;
+	int word_character_count = 0;
+	while((c = getchar()) != EOF)
+	{
+		++word_character_count;
+		if(c == ' ' || c == '\t' || c == '\n')
+		{
+			//Decrease the \0 string termination operator
+			//Decrease each occurrence of ' ', \t and \n
+			--word_character_count;
+			for(int i = 0; i < word_character_count; ++i)
+			{
+				putchar('#');
+				putchar('\n');
+			}
+			//Reset character count
+			word_character_count = 0;
+		}
+	}
+}
---
+++ chapter_1/test.c
@@ -0,0 +1,33 @@
+/*
+Test L 1-1
+File created: 2025-07-31
+File last modified: 2025-08-01
+Author: VesperVanity (VV) https://github.com/VesperVanity
+*/
+#include <stdio.h>
+
+int main(void)
+{
+	printf("%s\n", "qweqweqweqwe");
+	int number;
+	int number_2 = 0;
+	char first_number = 'b';
+	char first_character = 'c';
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
