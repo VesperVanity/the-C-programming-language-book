e29a7a30158148c88d564e37e3770ac2bddb7883
VesperVanity <OCEAN.jung@protonmail.com>
2025-08-01 17:18:50 +0200

    Exercise 1-13 vertically

--- chapter_1/histogram.c
+++ chapter_1/histogram.c
@@ -8,10 +8,11 @@ Author: VesperVanity (VV) https://github.com/VesperVanity
 #include <stdio.h>
 
 void histogram_horizontal();
+void histogram_vertical();
 
 int main(void)
 {
-	histogram_horizontal();
+	histogram_vertical();
 	return 0;
 }
 
@@ -36,6 +37,31 @@ void histogram_horizontal()
 			}
 			//Reset character count
 			word_character_count = 0;
+		}
+	}
+}
+
+void histogram_vertical()
+{
+	//Print histogram vertically depending on the length of words of the input
+	//Use arrays or counter variable
+	int c;
+	int word_character_count = 0;
+	while((c = getchar()) != EOF)
+	{
+		++word_character_count;
+		if(c == ' ' || c == '\t' || c == '\n')
+		{
+			//Decrease the \0 string termination operator
+			//Decrease each occurrence of ' ', \t and \n
+			--word_character_count;
+			for(int i = 0; i < word_character_count; ++i)
+			{
+				putchar('#');
+			}
+			//Reset character count
+			word_character_count = 0;
+			putchar('\n');
 		}
 	}
 }
