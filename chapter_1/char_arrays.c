b27304f14e47831823f71fac884cb9daaa32b95a
VesperVanity <OCEAN.jung@protonmail.com>
2025-08-04 18:47:13 +0200

    Start exercise 1-16

---
+++ chapter_1/char_arrays.c
@@ -0,0 +1,14 @@
+/*
+The C Programming Language by Brian W. Kernighan & Dennis M. Ritchie
+Chapter 1 Exercise - 1-16
+File created: 2025-08-04
+File last modified: 2025-08-04
+Author: VesperVanity (VV) https://github.com/VesperVanity
+*/
+#include <stdio.h>
+
+int main(void)
+{
+	
+	return 0;
+}
